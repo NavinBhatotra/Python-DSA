57434dee35a7d13efe7fe0e49db098c35167bb29
Navin Bhatotra <bhatotranavin@gmail.com>
2024-07-24 00:21:16 +0530

    Arrays Problems Best Score and 2D Diagonal Sum

---
+++ 2dDiagonalSum.py
@@ -0,0 +1,15 @@
+# Given 2D list calculate the sum of diagonal elements.
+
+# Example
+# myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+# diagonal_sum(myList2D) # 15
+
+def diagonal_sum(matrix):
+    # TODO
+    sum = 0
+    for i in range(len(matrix)):
+        sum += matrix[i][i]
+    return sum
+
+myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
+print(diagonal_sum(myList2D))
